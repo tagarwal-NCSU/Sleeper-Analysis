ec5509d7ded82e4b36fb57409138dbb3cb21014d
tagarwal-NCSU <tagarwa@ncsu.edu>
2022-01-11 08:53:14 -0500

    Flask Experimentation

--- Taylor/WebApp/FlaskDemo.py
+++ Taylor/WebApp/FlaskDemo.py
@@ -1,29 +1,35 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request,url_for
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    html = """
+    html = f"""
             <html>
+                <link rel= "stylesheet" type= "text/css" href="{ url_for('static',filename='style.css') }">
                 <h1>Sleeper Analysis</h1> 
-                <form action="/home"> 
+                <form action="/home" id="username"> 
                     <label for="username">Username: </label>
-                    <input type="text" id = "username" name = "username"></input> 
+                    <input type="text" id = "username" name = "username"></input>
+                    <br><br>
                     <input type="submit" value="Submit">
                 </form>
             </html>
             """
-    
-
     return html
 
 @app.route("/home")
 def home():
     username = request.args.get('username', "-")
     
+    html = f"""
+            <html>
+                <link rel= "stylesheet" type= "text/css" href="{ url_for('static',filename='style.css') }">
+                <h1>Your username is {username}</h1>
+            </html>
+            """
 
-    return f"<p>Your username is {username}</p>"
+    return html
 
 app.run(debug = True)
